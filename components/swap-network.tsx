38b599431c5b3febe0164b87c8ec5c989367a783
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-06-29 15:47:29 +0200

    A bit better

--- components/swap-network.tsx
+++ components/swap-network.tsx
@@ -22,7 +22,7 @@ export const SwapNetwork: React.FC<SwapNetworkProps> = ({
 
   return (
     <div className="relative mx-auto">
-      <button className="-top-5 -left-7 absolute rounded-full bg-[#232429] p-2 border-2 border-black" onClick={swapNetwork}>
+      <button className="-top-4 -left-7 absolute rounded-full bg-[#232429] p-2 border-2 border-black" onClick={swapNetwork}>
         <ChevronUpDownIcon className="h-7 w-7 text-gray-400" aria-hidden="true" />
       </button>
     </div>
